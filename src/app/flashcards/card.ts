fbd37f28b5c06c5187b57949913551a12454ad49
Yann Droy <yanndroy@yahoo.fr>
2025-01-25 16:07:18 -0600

    first working version

---
+++ src/app/flashcards/card.ts
@@ -0,0 +1,78 @@
+/** Enumeration of the Mandarin tones */
+enum Tone {
+  Neutral, First, Second, Third, Fourth
+}
+
+/** List of potential vowels for each Mandarin tone */
+const FIRST_TONE_VOWELS = ['ā', 'ē', 'ī', 'ū', 'ǖ', 'ō'];
+const SECOND_TONE_VOWELS = ['á', 'é', 'í', 'ú', 'ǘ', 'ó'];
+const THIRD_TONE_VOWELS = ['ǎ', 'ě', 'ǐ', 'ǔ', 'ǚ', 'ǒ'];
+const FOURTH_TONE_VOWELS = ['à', 'è', 'ì', 'ù', 'ǜ', 'ò'];
+
+/**
+ * Class that represents a flashcard in the application
+ */
+export class Card {
+  /** The Hanzi */
+  hanzi:string = "";
+
+  /** The Pinyin */
+  pinyin:string = "";
+
+  /** The English translation */
+  english:string = "";
+
+  /** Description (containing more information and/or example words) */
+  description:string = "";
+
+  /**
+   * Constructor
+   * @param hanzi The Hanzi
+   * @param pinyin The Pinyin
+   * @param english The English translation
+   * @param description The description
+   */
+  constructor(hanzi:string,
+              pinyin:string,
+              english:string,
+              description:string) {
+    this.hanzi = hanzi;
+    this.pinyin = pinyin;
+    this.english = english;
+    this.description = description;
+  };
+
+  /**
+   * Returns true if the card has a description, false otherwise
+   * @returns boolean
+   */
+  hasDescription():boolean {
+    return this.description.length > 0;
+  }
+
+  /**
+   * Returns the css class for the tone
+   * @returns string
+   */
+  toneClass():string {
+    if (FIRST_TONE_VOWELS.some(vowel => this.pinyin.includes(vowel))) {
+      return 'tone-1';
+    } else if (SECOND_TONE_VOWELS.some(vowel => this.pinyin.includes(vowel))) {
+      return 'tone-2';
+    } else if (THIRD_TONE_VOWELS.some(vowel => this.pinyin.includes(vowel))) {
+      return 'tone-3';
+    } else if (FOURTH_TONE_VOWELS.some(vowel => this.pinyin.includes(vowel))) {
+      return 'tone-4';
+    } else {
+      return 'tone-0';
+    }
+  }
+
+  /**
+   * Pretty prints the description with HTML tags for colors, etc.
+   * @returns a pretty string of the description
+   */
+  descriptionToHtml(): string {
+    return "<span></span>";//this.description;
+  }
+}
--- src/app/flashcards/flashcards.ts
+++ src/app/flashcards/flashcards.ts
@@ -1,11 +1,97 @@
+import { Card } from "./card";
+
+/** Enumeration of data types for each items in the .json file */
+enum Type {
+  Hanzi, Pinyin, Engligh, Description, None
+}
+
+/**
+ * Class that represents the flashcards component with its features
+ */
 export class Flashcards {
-    /**
-     * Constructor
-     * @param itemsFile content of src/assets/items.json
-     */
-    constructor(itemsFile: Array<Object>) {
-        for (let i:number = 0; i < itemsFile.length; i++) {
-            console.log(itemsFile[i]);
-        }
+  /** List of all the cards */
+  cards:Array<Card> = [];
+
+  /** Index of the current flashcard */
+  index:number = 0;
+
+  /** Boolean that indicates if Hanzi information is shown (Pinyin, translation, etc.) */
+  infoIsShown:boolean = false;
+
+  /**
+   * Constructor
+   * @param itemsFile content of src/assets/items.json
+   */
+  constructor(itemsFile: Array<Array<string>>) {
+    console.debug("--- Flashcards constructor: BEGIN");
+
+    // Loop through each item in the .json file
+    for (let i:number = 0; i < itemsFile.length; i++) {
+      console.debug("--- Flashcards constructor: processsing " + itemsFile[i]);
+
+      this.cards.push(
+        new Card(
+          itemsFile[i][Type.Hanzi],
+          itemsFile[i][Type.Pinyin],
+          itemsFile[i][Type.Engligh],
+          itemsFile[i][Type.Description]
+        )
+      );
     }
+
+    // Shuffle the cards
+    this.shuffleCards();
+    console.debug("--- Flashcards constructor: list of shuffled cards:");
+    console.debug(this.cards);
+
+    console.debug("--- Flashcards constructor: index: " + this.index);
+    console.debug("--- Flashcards constructor: infoIsShown: " + this.infoIsShown);
+
+    console.debug("--- Flashcards constructor: END");
+  }
+
+  /**
+   * Increments the index
+   */
+  nextCard():void {
+    this.index = (this.index + 1) % this.cards.length;
+    this.infoIsShown = false;
+
+    console.debug("--- Next card: index: " + this.index);
+    console.debug("--- Next card: infoIsShown: " + this.infoIsShown);
+  }
+
+  /**
+   * Toggles the boolean that controls the display of the Hanzi information
+   */
+  showInformation():void {
+    this.infoIsShown = true;
+
+    console.debug("--- Show information: infoIsShown: " + this.infoIsShown);
+  }
+
+  /**
+   * Returns true if the Hanzi information is shown, false otherwise
+   * @returns boolean
+   */
+  isInfoShown():boolean {
+    return this.infoIsShown;
+  }
+
+  /**
+   * Shuffles the array of cards
+   * @param cards the array to shuffle
+   * @returns a new array of the shuffled array
+   */
+  shuffleCards():void {
+    let shuffledArray:Array<Card> = [];
+    let length = this.cards.length;
+
+    while (length) {
+      let i:number = Math.floor(Math.random() * length--);
+      shuffledArray.push(this.cards.splice(i, 1)[0]);
+    }
+
+    this.cards = shuffledArray;
+  }
 }
